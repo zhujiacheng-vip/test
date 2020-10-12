344c7057b54f870b8476f6292af58a4c7147abd0
zjc <2484804005@qq.com>
2020-10-12 21:53:16 +0800

    提交Beans

---
+++ test-rabbitmq-consumer/src/main/java/com/xuecheng/test/rabbitmq/abc/Beans.java
@@ -0,0 +1,15 @@
+package com.xuecheng.test.rabbitmq.abc;
+
+/**
+ * @Author Mr zhu
+ * @Date 2020/10/12 21:52
+ */
+public class Beans {
+
+    public static void main(String[] args) {
+
+        System.out.println("我爱你");
+
+    }
+
+}
